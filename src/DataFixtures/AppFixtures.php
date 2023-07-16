5c71111cc639292d01fb3bde418191b40d420fc1
xtezza <74599219+xtezza@users.noreply.github.com>
2023-07-16 08:22:23 +0200

    Fixtures
    Fixtures for admins account

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,41 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\Entity\Team;
 
 class AppFixtures extends Fixture
 {
+    private $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        // admin account for admins (Angie and Xavier)
+        $team = new Team;
+        $team->setEmail('xavier.tezza@comnstay.fr');
+        $hashedPassword = $this->passwordHasher->hashPassword(
+            $team,
+            'Legion@2023'
+        );
+        $team->setPassword($hashedPassword);
+        $team->setRoles(['ROLE_SUPER_ADMIN']);
+        $manager->persist($team);
+
+        $team = new Team;
+        $team->setEmail('angie.racca.munoz@gmail.com');
+        $hashedPassword = $this->passwordHasher->hashPassword(
+            $team,
+            'Legion@2023'
+        );
+        $team->setPassword($hashedPassword);
+        $team->setRoles(['ROLE_SUPER_ADMIN']);
+        $manager->persist($team);
 
         $manager->flush();
     }

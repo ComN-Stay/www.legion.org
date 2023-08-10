f1af2781d7e0bdf5b7ee375220e3d64c9201a615
xtezza <74599219+xtezza@users.noreply.github.com>
2023-08-10 08:53:15 +0200

    update migrations

--- migrations/Version20230723103452.php
+++ migrations/Version20230723103452.php
@@ -19,6 +19,28 @@ final class Version20230723103452 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
+        $this->addSql('CREATE TABLE `company` (
+            `id` int NOT NULL,
+            `fk_company_type_id` int NOT NULL,
+            `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+            `email` varchar(80) COLLATE utf8mb4_unicode_ci NOT NULL,
+            `phone` varchar(25) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+            `address` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+            `additional_address` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+            `zip_code` varchar(15) COLLATE utf8mb4_unicode_ci NOT NULL,
+            `town` varchar(80) COLLATE utf8mb4_unicode_ci NOT NULL,
+            `status` tinyint(1) NOT NULL,
+            `longitude` double DEFAULT NULL,
+            `latitude` double DEFAULT NULL,
+            `logo` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+            `facebook` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+            `tweeter` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+            `instagram` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+            `description` longtext COLLATE utf8mb4_unicode_ci,
+            `short_description` longtext COLLATE utf8mb4_unicode_ci
+          ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;');
+        $this->addSql('ALTER TABLE `company` ADD PRIMARY KEY (`id`), ADD KEY `IDX_4FBF094F3CBDC271` (`fk_company_type_id`);');
+        $this->addSql('ALTER TABLE `company` MODIFY `id` int NOT NULL AUTO_INCREMENT;');
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE company ADD CONSTRAINT FK_4FBF094F3CBDC372 FOREIGN KEY (fk_company_type_id) REFERENCES company_type (id)');
     }
